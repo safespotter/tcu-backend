04ee34e6fffa1ffe397ec27ae4a65362cdb8435a
Stefano R. Usai <stefano.r.usai@gmail.com>
2020-05-13 11:31:23 +0200

    refactor

--- config/routes.js
+++ config/routes.js
@@ -3,16 +3,9 @@ const DashMan = require('../engine/dashboard-manager');
 const CalMan = require('../engine/calendar-manager');
 
 const ErrorHandler = require('../engine/error-handler');
-const VideoMan= require('../engine/SocketEmit');
 
 module.exports = function (app, passport, config) {
-    // const socket = require("socket.io");
-    //
-    // const server = app.listen(3000, () => {
-    //     console.log('started in 3000')
-    // });
-    //
-    // const io = socket(server);
+
     const site_URL = (config['site_URL'].includes('localhost') ? 'http://localhost:4200' : '') + '/#/preferences/api-keys?err=true';
 
     /* PATHs */
@@ -43,13 +36,6 @@ module.exports = function (app, passport, config) {
     app.delete(`${amPath}/delete/`, reqAuth, AccMan.roleAuth([admin]), AccMan.deleteUser);
 
 
-    /****************** TOKENS ********************/
-
-    /****************** CRUD DASHBOARD ********************/
-
-    /****************** CRUD MESSAGES ********************/
-
-
     /****************** CALENDAR MANAGER ******************/
     app.get(`${calPath}/getEvents`, reqAuth, AccMan.roleAuth(all), CalMan.getEvents);
     app.post(`${calPath}/addEvent`, reqAuth, AccMan.roleAuth(all), CalMan.addEvent);
@@ -58,7 +44,75 @@ module.exports = function (app, passport, config) {
 
     /****************** SOCKET IO ******************/
 
-    app.post('/SafeSpotter/create',VideoMan.getData)
+    const socket = require("socket.io");
+
+    const server = app.listen(3000, () => {
+        console.log('started in 3000')
+    });
+
+    const io = socket(server);
+
+    var Charts = require('../models/mongo/chart');
+    var SafeSpotter = require('../models/mongo/mongo-safeSpotter')
+    var socketMap = [];
+
+    io.on('connection',(socket)=>{
+        console.log("Client Connected");
+        socketMap.push(socket);
+        dataUpdate();
+    });
+
+    app.post('/SafeSpotter/create', function (req, res) {
+        let tmp_critical;
+        let allert;
+        let id;
+
+        (async () => {
+            try {
+                console.log("Calling for chart Create");
+                console.log(req.body);
+                // dati su mongo
+
+                if((await SafeSpotter.find({id: req.body.id })).length != 0  &&  req.body.critical_issues  >= 0 && req.body.critical_issues <=5 ) {
+                    tmp_critical = await SafeSpotter.find({id: req.body.id });
+                    tmp_critical[0].critical_issues != req.body.critical_issues ? id = req.body.id : id = -1;
+                    req.body.critical_issues  == 5 ? allert = 1 : allert = 0;
+
+                    await SafeSpotter.updateOne({id: req.body.id},
+                        {street: req.body.street,
+                            ip: req.body.ip,
+                            critical_issues: req.body.critical_issues,
+                            date: new Date()})
+                }else{
+                    console.log('entro qui')
+                    let safeSpotter = new SafeSpotter(req.body)
+                    await safeSpotter.save();
+                }
+
+                //dati su mongo
+                dataUpdate(id, allert); //richiamo l'emissione
+                res.json("Charts  Successfully Created"); //parse
+            } catch (err) {
+                console.log(err);
+                //res.status(400).send(err);
+            }
+        })();
+    });
+
+
+    async function dataUpdate(num, allert){
+        console.log('Socket Emmit');
+        var charts = await Charts.find({});
+        var safespotter = await SafeSpotter.find().sort({date:-1});
+        for(let socketMapObj of socketMap){
+            if(safespotter.length > 0){
+                socketMapObj.emit('dataUpdate',[
+                    safespotter, num, allert]);
+            }
+        }
+
+
+    }
 
     /****************** ERROR HANDLER ********************/
     app.use(ErrorHandler.fun404);
--- engine/SocketEmit.js
+++ engine/SocketEmit.js
@@ -1,71 +1,71 @@
-'use strict';
-const express = require('express');
-
-const app = express();
-const server = app.listen(3000, () => {
-    console.log('started in 3000')
-});
-
-
-    const socket = require("socket.io");
-    const io = socket(server);
-
-    var SafeSpotter = require('../models/mongo/mongo-safeSpotter')
-    var socketMap = [];
-
-    io.on('connection', (socket) => {
-        console.log("Client Connected");
-        socketMap.push(socket);
-        dataUpdate();
-    });
-
-    async function getData(req) {
-
-
-        let tmp_critical;
-        let allert;
-        let id;
-        try {
-
-            if ((await SafeSpotter.find({id: req.body.id})).length != 0 && req.body.critical_issues >= 0 && req.body.critical_issues <= 5) {
-                tmp_critical = await SafeSpotter.find({id: req.body.id});
-                tmp_critical[0].critical_issues != req.body.critical_issues ? id = req.body.id : id = -1;
-                req.body.critical_issues == 5 ? allert = 1 : allert = 0;
-
-                await SafeSpotter.updateOne({id: req.body.id},
-                    {
-                        street: req.body.street,
-                        ip: req.body.ip,
-                        critical_issues: req.body.critical_issues,
-                        date: new Date()
-                    })
-            } else {
-                console.log('entro qui')
-                let safeSpotter = new SafeSpotter(req.body)
-                await safeSpotter.save();
-            }
-
-            //dati su mongo
-            dataUpdate(id, allert); //richiamo l'emissione
-            res.json("Charts  Successfully Created"); //parse
-        } catch (err) {
-            console.log(err);
-            //res.status(400).send(err);
-        }
-    }
-
-
-    async function dataUpdate(num, allert) {
-        console.log('Socket Emmit');
-        var safespotter = await SafeSpotter.find().sort({date: -1});
-        for (let socketMapObj of socketMap) {
-            if (safespotter.length > 0) {
-                socketMapObj.emit('dataUpdate', [
-                    safespotter, num, allert]);
-            }
-        }
-
-
-    }
-
-    module.exports = {getData}
+// 'use strict';
+// const express = require('express');
+//
+// const app = express();
+// const server = app.listen(3000, () => {
+//     console.log('started in 3000')
+// });
+//
+//
+//     const socket = require("socket.io");
+//     const io = socket(server);
+//
+//     var SafeSpotter = require('../models/mongo/mongo-safeSpotter')
+//     var socketMap = [];
+//
+//     io.on('connection', (socket) => {
+//         console.log("Client Connected");
+//         socketMap.push(socket);
+//         dataUpdate();
+//     });
+//
+//     app.post('/SafeSpotter/create', async function (req) {
+//             console.log(req.body)
+//
+//         let tmp_critical;
+//         let allert;
+//         let id;
+//         try {
+//
+//             if ((await SafeSpotter.find({id: req.body.id})).length != 0 && req.body.critical_issues >= 0 && req.body.critical_issues <= 5) {
+//                 tmp_critical = await SafeSpotter.find({id: req.body.id});
+//                 tmp_critical[0].critical_issues != req.body.critical_issues ? id = req.body.id : id = -1;
+//                 req.body.critical_issues == 5 ? allert = 1 : allert = 0;
+//
+//                 await SafeSpotter.updateOne({id: req.body.id},
+//                     {
+//                         street: req.body.street,
+//                         ip: req.body.ip,
+//                         critical_issues: req.body.critical_issues,
+//                         date: new Date()
+//                     })
+//             } else {
+//                 console.log('entro qui')
+//                 let safeSpotter = new SafeSpotter(req.body)
+//                 await safeSpotter.save();
+//             }
+//
+//             //dati su mongo
+//             dataUpdate(id, allert); //richiamo l'emissione
+//             res.json("Charts  Successfully Created"); //parse
+//         } catch (err) {
+//             console.log(err);
+//             //res.status(400).send(err);
+//         }
+//     });
+//
+//
+//     async function dataUpdate(num, allert) {
+//         console.log('Socket Emmit');
+//         var safespotter = await SafeSpotter.find().sort({date: -1});
+//         for (let socketMapObj of socketMap) {
+//             if (safespotter.length > 0) {
+//                 socketMapObj.emit('dataUpdate', [
+//                     safespotter, num, allert]);
+//             }
+//         }
+//
+//
+//     }
+//
+//     module.exports = {getData}
